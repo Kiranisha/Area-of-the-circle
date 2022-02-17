fa53b8e305ec684ff460442d71446ea0cd542a2b
Kiranisha <99899774+Kiranisha@users.noreply.github.com>
2022-02-17 22:26:01 +0530

    created area of the circle.py

---
+++ Area of the circle.py
@@ -0,0 +1,8 @@
+Python 3.10.0a1 (tags/v3.10.0a1:8e9afaf, Oct  5 2020, 20:32:52) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> r = float(input("Input the radius of the circle : "))
+Input the radius of the circle : 1.1
+>>> pi=3.14
+>>> print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+The area of the circle with radius 1.1 is: 3.7994000000000008
+>>> 
